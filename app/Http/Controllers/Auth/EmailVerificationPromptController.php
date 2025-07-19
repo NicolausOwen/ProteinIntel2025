683a0c12f5cc854f745550f89603971d77d56dc1
NicolausOwen <ww051205@gmail.com>
2025-07-19 20:08:00 +0700

    feat: UserPanel
    -Added UserPanelProvider
    -Added User dashboard page
    -refactor redirects to filament user dashboard

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -15,7 +15,8 @@ class EmailVerificationPromptController extends Controller
     public function __invoke(Request $request): RedirectResponse|View
     {
         return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(route('dashboard', absolute: false))
-                    : view('auth.verify-email');
+            // ? redirect()->intended(route('dashboard', absolute: false))
+            ? redirect()->intended(route('filament.user.pages.dashboard'))
+            : view('auth.verify-email');
     }
 }
---
+++ resources/views/filament/user/pages/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-filament-panels::page>
+    <h1 class="text-xl font-bold">Selamat datang di Dashboard User</h1>
+</x-filament-panels::page>
