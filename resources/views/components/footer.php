c146056a17646128066437327d0a987dd67d9bbb
hhherdian <rafiherdian2@gmail.com>
2025-08-03 02:15:37 +0700

    Add files via upload

---
+++ resources/views/components/footer.php
@@ -0,0 +1,72 @@
+<head>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+    body {
+      font-family: 'Montserrat', sans-serif;
+    }
+  </style>
+</head>
+
+<footer class="bg-[#333446] text-white">
+  <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    <div class="md:flex md:justify-between">
+      <div class="mb-6 md:mb-0">
+        <span class="self-center text-2xl font-semibold whitespace-nowrap">Protein</span>
+      </div>
+      <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+        <div>
+          <h2 class="mb-6 text-sm font-semibold uppercase">Resources</h2>
+          <ul class="text-gray-300 font-medium">
+            <li class="mb-4">
+              <a href="https://intelunsri.com" class="hover:text-[#B8CFCE]">Intel Unsri</a>
+            </li>
+            <li>
+              <a href="https://protein.com" class="hover:text-[#B8CFCE]">Protein</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 class="mb-6 text-sm font-semibold uppercase">Follow us</h2>
+          <ul class="text-gray-300 font-medium">
+            <li class="mb-4">
+              <a href="https://www.youtube.com/" class="hover:text-[#B8CFCE]">YouTube 1</a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/" class="hover:text-[#B8CFCE]">YouTube 2</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 class="mb-6 text-sm font-semibold uppercase">More</h2>
+          <ul class="text-gray-300 font-medium">
+            <li class="mb-4">
+              <a href="#" class="hover:text-[#B8CFCE]">Lorem 1</a>
+            </li>
+            <li>
+              <a href="#" class="hover:text-[#B8CFCE]">Lorem 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <hr class="my-6 border-gray-500 sm:mx-auto" />
+    <div class="sm:flex sm:items-center sm:justify-between">
+      <span class="text-sm text-gray-300">2025 <a href="#" class="hover:text-[#B8CFCE]">Intel Unsri</a></span>
+      <div class="flex mt-4 sm:justify-center sm:mt-0">
+        <a href="https://www.instagram.com/intelunsri/" class="text-gray-300 hover:text-[#B8CFCE] ms-5" target="_blank">
+          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2Zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm4.75-.88a.88.88 0 1 1 0 1.76.88.88 0 0 1 0-1.76Z"/>
+          </svg>
+          <span class="sr-only">Instagram</span>
+        </a>
+        <a href="https://www.tiktok.com/@intelunsri" class="text-gray-300 hover:text-[#B8CFCE] ms-5" target="_blank">
+          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+            <path d="M41,14.11c-3.64,0-6.88-1.44-9.37-3.76v14.1c0,7.12-5.92,12.96-13.21,12.45c-6.46-0.45-11.78-5.77-12.23-12.23c-0.54-7.3,5.33-13.31,12.44-13.31c0.4,0,0.79,0.02,1.17,0.05v7.9c-0.38-0.07-0.77-0.11-1.17-0.11c-2.87,0-5.2,2.33-5.2,5.2s2.33,5.2,5.2,5.2s5.2-2.33,5.2-5.2V2h7.46c0.47,5.97,5.28,10.7,11.25,11.17V14.11z"/>
+          </svg>
+          <span class="sr-only">TikTok</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
