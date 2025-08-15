d2edabdba31c933d599bb84ae5eab9edecc50a56
NicolausOwen <ww051205@gmail.com>
2025-08-15 17:22:55 +0700

    Updated User Panel Colors

--- app/Providers/Filament/UserPanelProvider.php
+++ app/Providers/Filament/UserPanelProvider.php
@@ -28,7 +28,22 @@ class UserPanelProvider extends PanelProvider
             ->default()
             ->path('user')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#2C2C3C',
+                'gray' => [
+                    50 => '250, 250, 250',
+                    100 => '244, 244, 245',
+                    200 => '228, 228, 231',
+                    300 => '212, 212, 216',
+                    400 => '161, 161, 170',
+                    500 => '113, 113, 122',
+                    600 => '82, 82, 91',
+                    700 => '63, 63, 70',
+                    800 => '39, 39, 42',
+                    900 => '24, 24, 27',
+                    950 => '6, 6, 8',
+                ],
+                'accent' => '#C2D8D8',
+                'muted' => '#F7FAFC',
             ])
             ->discoverResources(in: app_path('Filament/User/Resources'), for: 'App\\Filament\\User\\Resources')
             ->discoverPages(in: app_path('Filament/User/Pages'), for: 'App\\Filament\\User\\Pages')
