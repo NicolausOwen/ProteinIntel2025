dde9ff099742011ad27e32d141e80884b530b579
NicolausOwen <ww051205@gmail.com>
2025-08-19 01:48:10 +0700

    Fixed Typo

--- app/Http/Controllers/User/QuizController.php
+++ app/Http/Controllers/User/QuizController.php
@@ -38,7 +38,7 @@ class QuizController extends Controller
         $quiz = Quiz::select('id', 'title', 'description', 'duration_minutes')->find($quizId);
         session(['quiz_data' => ['duration_minutes' => $quiz->duration_minutes]]);
 
-        return view('pages/quiz/quiZDetail', compact('quiz'));
+        return view('pages/quiz/quizDetail', compact('quiz'));
     }
 
 
