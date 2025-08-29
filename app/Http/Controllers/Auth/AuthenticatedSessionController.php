6f141ec7818a2dd8b5fbc5d847a03acd5ca3a7f2
Muhammad Dzaky Hasyim <muhammadzakiplg@gmail.com>
2025-08-30 00:32:20 +0700

    fixed: role admin redirected to user dashboard instead of admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         // return redirect()->intended(route('dashboard', absolute: false));
-        return redirect()->intended(route('filament.user.pages.dashboard'));
+        return redirect()->intended(route('filament.admin.pages.dashboard'));
 
     }
 
